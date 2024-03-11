462bc18d6c571f911472b595a5ce2ef4bb4532ab
amimanye <amimanyeaglago@gmail.com>
2024-03-11 04:40:48 +0000

    removed the importing of usecontext from component A

--- useContext/src/components/componentA.jsx
+++ useContext/src/components/componentA.jsx
@@ -1,5 +1,5 @@
 import ComponentB from "./componentB";
-import { createContext, useContext, useState } from "react";
+import { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
